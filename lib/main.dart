f44b658a1da6ee96de5b57af27fe0636eeecac89
Alaa Hassan <alaa.hassan200362@gmail.com>
2026-05-15 04:04:20 +0300

    creating cluster bridge between dart and zenohc

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,41 @@
 
 
 
+// import 'package:flutter/material.dart';
+// import 'package:flutter_cluster/screen/cluster_screen.dart';
+// import 'package:provider/provider.dart';
+// import 'services/cluster_client.dart';
+// import 'provider/cluster_provider.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   final client = ClusterClient(keyExpr: 'autoware/cluster');
+
+//   runApp(
+//     ChangeNotifierProvider(
+//       create: (_) => VehicleFrameNotifier(client),
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Vehicle Cluster',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData.dark(),
+//       home: const ClusterScreen(),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_cluster/screen/cluster_screen.dart';
 import 'package:provider/provider.dart';
@@ -33,7 +68,12 @@ import 'provider/cluster_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final client = ClusterClient(keyExpr: 'autoware/cluster');
+  final client = ClusterClient(
+    // locator: 'udp/127.0.0.1:7447',  // ADDED
+    // keyExpr: 'autoware/cluster',
+     locator: 'udp/127.0.0.1:7447',          // empty = peer mode, no explicit connect
+  keyExpr: 'autoware/cluster',
+  );
 
   runApp(
     ChangeNotifierProvider(
